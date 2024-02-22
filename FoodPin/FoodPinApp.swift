49c09006868f282503a5a94590019cb53b68761a
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2024-02-22 23:20:20 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by donghs on 2024/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
